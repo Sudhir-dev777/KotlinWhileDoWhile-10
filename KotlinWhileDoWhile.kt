1e189b656ef01f246cc9fed396a914587d705e35
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2020-10-10 22:53:03 +0500

    while loop
    basics

---
+++ KotlinWhileDoWhile.kt
@@ -0,0 +1,27 @@
+
+ //Kotlin While and Do While Loop
+fun main(args : Array<String>){
+   While Loop same as java, c
+   var a = 1
+     while(a<=5)
+     {
+         println(a)
+         a++
+     }
+
+     //Kotlin infinite While Loop
+   /*  while (true)
+     {
+         println("Hello while infinity!")
+     }
+
+    */
+     // Do While in Kolin
+     var b = 6
+     do
+     {
+         println(b)
+         b++
+     }
+     while(b<=5);
+ }
